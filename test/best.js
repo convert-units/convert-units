d75a99b55a55988f1957acb956ab1a67b7530be8
Maddi Joyce <maddi@maddijoyce.com>
2015-09-16 13:12:11 +1000

    Adding best function for autoselecting to unit.

---
+++ test/best.js
@@ -0,0 +1,15 @@
+var convert = require('../lib')
+  , assert = require('assert')
+  , tests = {};
+
+tests['best mm'] = function () {
+  var actual = convert(1200).from('mm').best()
+    , expected = {
+        val: 1.2
+      , unit: 'm'
+      };
+
+  assert.deepEqual(actual, expected);
+};
+
+module.exports = tests;
