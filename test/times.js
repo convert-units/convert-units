d19a0a3d20e6ba1ec720af36235e8ac59cc6307f
John Purcell <john.purcell@envolvehealth.com>
2016-10-18 14:44:20 -0400

    add more time unit tests

--- test/times.js
+++ test/times.js
@@ -27,10 +27,33 @@ tests['s to d'] = function () {
 
 tests['d to week'] = function () {
   assert.strictEqual( convert(7).from('d').to('week'), 1);
-}
+};
 
-tests['w to year'] = function () {
+// Incorrect, shoud be either 30.4167 or 30.4375 days in a month
+tests ['d to month'] = function () {
+  assert.strictEqual( convert(28).from('d').to('month'), 1);
+};
+
+// Incorrect, should be 365 or 365.25 days in 1 year
+tests ['d to year'] = function () {
+  assert.strictEqual( convert(364).from('d').to('year'), 1);
+};
+
+// Incorrect, should be 4.34524 or 4.34821 weeks in 1 month
+tests['week to month'] = function () {
+  assert.strictEqual( convert(4).from('week').to('month'), 1);
+};
+
+tests['week to year'] = function () {
   assert.strictEqual( convert(52).from('week').to('year'), 1);
+};
+
+// Incorrect, should be 12 months in 1 year
+tests['month to year'] = function () {
+  var expected = .923
+    , actual = convert(12).from('month').to('year');
+  assert.ok( percentError(expected, actual) < ACCURACY
+    , 'Expected: ' + expected +', Actual: ' + actual);
 };
 
 module.exports = tests;
