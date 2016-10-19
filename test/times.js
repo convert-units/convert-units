8dd137b5a3fe68a1fb6db87fd74e4827ee516068
John Purcell <john.purcell@gmail.com>
2016-10-18 21:31:30 -0400

    fix month and year calculations

--- test/times.js
+++ test/times.js
@@ -29,31 +29,30 @@ tests['d to week'] = function () {
   assert.strictEqual( convert(7).from('d').to('week'), 1);
 };
 
-// Incorrect, shoud be either 30.4167 or 30.4375 days in a month
 tests ['d to month'] = function () {
-  assert.strictEqual( convert(28).from('d').to('month'), 1);
+  assert.strictEqual( convert(30.4375).from('d').to('month'), 1);
 };
 
-// Incorrect, should be 365 or 365.25 days in 1 year
 tests ['d to year'] = function () {
-  assert.strictEqual( convert(364).from('d').to('year'), 1);
+  assert.strictEqual( convert(365.25).from('d').to('year'), 1);
 };
 
-// Incorrect, should be 4.34524 or 4.34821 weeks in 1 month
 tests['week to month'] = function () {
-  assert.strictEqual( convert(4).from('week').to('month'), 1);
+  var expected = 1
+    , actual = convert(4.34821).from('week').to('month');
+  assert.ok( percentError(expected, actual) < ACCURACY
+    , 'Expected: ' + expected +', Actual: ' + actual);
 };
 
 tests['week to year'] = function () {
-  assert.strictEqual( convert(52).from('week').to('year'), 1);
-};
-
-// Incorrect, should be 12 months in 1 year
-tests['month to year'] = function () {
-  var expected = .923
-    , actual = convert(12).from('month').to('year');
+  var expected = 1
+    , actual = convert(52.17857).from('week').to('year');
   assert.ok( percentError(expected, actual) < ACCURACY
     , 'Expected: ' + expected +', Actual: ' + actual);
+};
+
+tests['month to year'] = function () {
+  assert.strictEqual( convert(12).from('month').to('year'), 1);
 };
 
 module.exports = tests;
