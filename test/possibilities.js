837d00f0cdfbce53685cc426a1c31e7985c52401
Craigzyc <craig.zych@neelands.com>
2017-06-05 07:55:37 -0400

    Update possibilities.js

--- test/possibilities.js
+++ test/possibilities.js
@@ -54,7 +54,7 @@ tests['length possibilities'] = function () {
 
 tests['temperature possibilities'] = function () {
   var actual = convert().possibilities('temperature')
-    , expected = ['C', 'K', 'F'];
+    , expected = ['C', 'K', 'F', 'R'];
   assert.deepEqual(actual.sort(), expected.sort())
 }
 
