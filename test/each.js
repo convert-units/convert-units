4203bed047b96632390e3af4330b3d7a07309e2c
Jonathan Muribeca <jonathanmuribeca@gmail.com>
2017-06-23 15:23:18 -0300

    Added dozen definition

--- test/each.js
+++ test/each.js
@@ -6,4 +6,12 @@ tests['ea to ea'] = function () {
   assert.strictEqual( convert(1).from('ea').to('ea') , 1);
 };
 
+tests['dz to ea'] = function () {
+  assert.strictEqual( convert(1).from('dz').to('ea') , 12);
+};
+
+tests['ea to dz'] = function () {
+  assert.strictEqual( convert(12).from('ea').to('dz') , 1);
+};
+
 module.exports = tests;
