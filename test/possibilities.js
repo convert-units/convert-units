f5b1025c80f60412e4f5e46165f9918788d6da02
jessie <jessie.su@farmersedge.ca>
2017-11-16 15:59:15 -0800

    Updated imperial ton symbol to 'ton (US)'

--- test/possibilities.js
+++ test/possibilities.js
@@ -12,7 +12,7 @@ tests['l possibilities'] = function () {
 
 tests['kg possibilities'] = function () {
   var actual = convert().from('kg').possibilities()
-    , expected = [ 'mcg', 'mg', 'g', 'kg', 't', 'oz', 'lb', 't' ];
+    , expected = [ 'mcg', 'mg', 'g', 'kg', 't', 'oz', 'lb', 'ton (US)' ];
   assert.deepEqual(actual.sort(), expected.sort())
 };
 
@@ -30,7 +30,7 @@ tests['each possibilities'] = function () {
 
 tests['mass possibilities'] = function () {
   var actual = convert().possibilities('mass')
-    , expected = [ 'mcg', 'mg', 'g', 'kg', 't', 'oz', 'lb', 't' ];
+    , expected = [ 'mcg', 'mg', 'g', 'kg', 't', 'oz', 'lb', 'ton (US)' ];
   assert.deepEqual(actual.sort(), expected.sort())
 };
 
@@ -280,7 +280,7 @@ tests['all possibilities'] = function () {
       , 'Tbs'
       , 'Tbs/s'
       , 't'
-      , 't'
+      , 'ton (US)'
       , 'torr'
       , 'tsk'
       , 'tsp'
