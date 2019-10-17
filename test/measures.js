b39c8bed94f1c50052859f57e1c03962e637c512
Stephane Nguyen <snguyen@sierrawireless.com>
2019-10-17 12:04:00 -0700

    kb KB etc separately

--- test/measures.js
+++ test/measures.js
@@ -4,7 +4,7 @@ var convert = require('../lib')
 
 tests['measures'] = function () {
   var actual = convert().measures()
-    , expected = [ 'length', 'area', 'mass', 'volume', 'each', 'temperature', 'time', 'digital', 'partsPer', 'speed', 'pace', 'pressure', 'current', 'voltage', 'power', 'reactivePower', 'apparentPower', 'energy', 'reactiveEnergy', 'volumeFlowRate', 'illuminance', 'frequency', 'angle', 'charge', 'force', 'acceleration' ];
+    , expected = [ 'length', 'area', 'mass', 'volume', 'each', 'temperature', 'time', 'digital', 'bits', 'bitspersecond', 'bytes', 'bytespersecond', 'bibytes', 'bibytespersecond', 'bibits', 'bibitspersecond', 'partsPer', 'speed', 'pace', 'pressure', 'current', 'voltage', 'power', 'reactivePower', 'apparentPower', 'energy', 'reactiveEnergy', 'volumeFlowRate', 'illuminance', 'frequency', 'angle', 'charge', 'force', 'acceleration' ];
   assert.deepEqual(actual, expected);
 };
 
