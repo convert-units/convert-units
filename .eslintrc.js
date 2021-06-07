923ce8deba50205fc2fbc63ee29d6e04913c735f
Randy Topliffe <randytopliffe@gmail.com>
2021-06-07 10:55:46 -0400

    Ordered Imports
    - Add eslint rules for ordered imports
    - Add prettier plugin to auto format imports. Added this because I don't
    like managing imports :E
    
    The general idea is to decrease code maintance overhead.

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     'prettier',
+    'plugin:ordered-imports/recommended',
   ],
   parserOptions: {
     ecmaVersion: 12,
@@ -19,9 +20,27 @@ module.exports = {
   plugins: [
     'jest',
     'prettier',
+    'ordered-imports',
   ],
   rules: {
     'prettier/prettier': 2,
+    'ordered-imports/ordered-imports': [
+      'error',
+      {
+        'symbols-first': true,
+        'declaration-ordering': ['type', {
+          ordering: ['side-effect', 'default', 'namespace', 'destructured'],
+          secondaryOrdering: ['source', 'lowercase-last'],
+        }],
+        'specifier-ordering': 'lowercase-last',
+        'group-ordering': [
+          { name: 'project root', match: '^@', order: 20 },
+          { name: 'parent directories', match: '^\\.\\.', order: 30 },
+          { name: 'current directory', match: '^\\.', order: 40 },
+          { name: 'third-party', match: '.*', order: 10 },
+        ],
+      },
+    ],
   },
   parser: '@typescript-eslint/parser',
 };
